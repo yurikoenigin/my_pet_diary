c2f71093ed9c0ed8cd4f1ca8a6d3f61766e86694
Yuriko Kojima <yuri@YurikonoMacBook-Air.local>
2026-02-16 15:59:35 +0900

    Deviseの日本語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # 新規登録時にnameを許容
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     # アカウント編集時にnameを許容
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 end
