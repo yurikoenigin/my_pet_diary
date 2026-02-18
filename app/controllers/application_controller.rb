520f7e8e9f20e9b8cadb834688a780c3a867d1d5
Yuriko Kojima <yuri@YurikonoMacBook-Air.local>
2026-02-18 17:27:14 +0900

    petsテーブルにサンプルデータ登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
+  def after_sign_in_path_for(resource)
+    pets_path 
+  end
+  
   protected
 
   def configure_permitted_parameters
