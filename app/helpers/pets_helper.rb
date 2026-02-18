6b0c7aa242b64aa203f191383f23c4c9e188a213
Yuriko Kojima <yuri@YurikonoMacBook-Air.local>
2026-02-18 22:16:05 +0900

    Pet一覧ページの調整

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,2 +1,12 @@
 module PetsHelper
+  def gender_class(gender)
+    case gender
+    when 'male'
+      'bg-blue-100 text-blue-700'
+    when 'female'
+      'bg-pink-100 text-pink-700'
+    else
+      'bg-gray-100 text-gray-700'
+    end
+  end
 end
