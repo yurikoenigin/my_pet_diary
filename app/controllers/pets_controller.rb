4c4656214b353c79fcb466d310a9b27c1d7e920c
Yuriko Kojima <yuri@YurikonoMacBook-Air.local>
2026-02-19 17:29:24 +0900

    pet登録機能

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_pet, only: %i[ show ]
+  before_action :set_pet, only: %i[ show edit update destroy ]
 
   def index
     @pets = current_user.pets
@@ -8,10 +8,38 @@ class PetsController < ApplicationController
 
   def show; end
 
+  def new
+    @pet = current_user.pets.build
+  end
+
+  def edit; end
+
+  def create
+    @pet = current_user.pets.build(pet_params)
+    if @pet.save
+      redirect_to pets_path, notice: "ペットを登録しました！"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to pets_path, notice: "ペット情報を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path, notice: "ペットを削除しました。"
+  end
+
   private
 
   def set_pet
-    @pet = current_user.pets.find(params[id])
+    @pet = current_user.pets.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to pets_path, alert: "アクセス権限がありません。"
   end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -10,4 +10,13 @@ class Pet < ApplicationRecord
   validates :image, content_type: ACCEPTED_CONTENT_TYPES, size: { less_than_or_equal_to: 5.megabytes }
 
   enum :gender, { male: 0, female: 1, other: 2 }
+
+  def age
+    return nil if birthday.blank?
+
+    # 今日の日付と誕生日の差から年齢を計算
+    d1 = birthday.strftime("%Y%m%d").to_i
+    d2 = Time.zone.now.strftime("%Y%m%d").to_i
+    (d2 - d1) / 10000
+  end
 end
